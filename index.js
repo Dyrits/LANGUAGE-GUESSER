96c40c1da9d5ef87b65bc3dab48adae2f619174a
Dyrits <dyrits@outlook.com>
2022-04-06 18:18:23 -0500

    Argument renamed.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { log } = console;
 
 const getLanguage = (code) => langs.where("3", code);
 
-const language = getLanguage(francAll(input).find(([language]) => getLanguage(language))?.[0])?.name;
+const language = getLanguage(francAll(input).find(([code]) => getLanguage(code))?.[0])?.name;
 
 log(language ? `The language is probably ${language}.` : `Could not match a language. Please try again.`);
 
